fbd9961f0ebcc2af3cb1bd84da22ff49b8835b91
Galen Guyer <git@galenguyer.com>
2020-06-05 18:06:45 -0700

    Add root url handler

--- index.php
+++ index.php
@@ -18,8 +18,47 @@ if (!(file_exists("./animals/$uri") && is_dir("./animals/$uri")))
 	die();
 }
 
+# Root url
+if ($uri == "/"){
+	$cat_count = count(glob("./animals/cats/*"));
+	$possum_count = count(glob("./animals/possums/*"));
+	$raccoon_count = count(glob("./animals/raccoons/*"));
+	
+	echo <<<EOT
+	<html lang='en'>
+	<head>
+	<title>Random Animals!</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="Galen Guyer">
+	<meta name="description" content="Get a bunch of random animal pics!" />
+	<style>
+		body {
+			font-family: Arial, Helvetica, sans-serif;
+			text-align: center;
+		}
+		img {
+			max-width: 90vw;
+			max-height: 70vh;
+		}
+	</style>
+	</head>
+
+	<body>
+	<header>
+		<h1>Hey look! It's a bunch of random animals!</h1>
+	</header>
+	<h3>Currently serving:</h3>
+	<p><a href="/cats/">$cat_count cats!</a></p>
+	<p><a href="/possums/">$possum_count possums!</a></p>
+	<p><a href="/raccoons/">$raccoon_count raccoons!</a></p>
+	</body>
+	</html>
+	EOT;
+	die();
+}
 # Raw endpoint
-if (trim($uri, "/") == ($base . "/raw")) {
+else if (trim($uri, "/") == ($base . "/raw")) {
 	$files = glob("./animals/$base/*");
 	$file = substr($files[array_rand($files)], 2);
 	header("Content-Type: text/plain");
